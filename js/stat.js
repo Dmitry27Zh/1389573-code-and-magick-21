4ec922094e164451a70daab97d1506b2cefcd4c0
Dmitry <dmitry27zh@gmail.com>
2020-09-21 21:00:31 +0300

    закончена гистограмма

--- js/stat.js
+++ js/stat.js
@@ -18,18 +18,6 @@ const renderRect = function (ctx, color, x, y, width, height) {
   ctx.fillRect(x, y, width, height);
 };
 
-const getMaxElement = function (arr) {
-  let MaxElement = arr[0];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > MaxElement) {
-      MaxElement = arr[i];
-    }
-  }
-
-  return MaxElement;
-};
-
 window.renderStatistics = function (ctx, names, times) {
   renderRect(ctx, `rgba(0, 0, 0, 0.7)`, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
   renderRect(ctx, `#fff`, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -38,25 +26,29 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = `${FONT_SIZE}px PT Mono`;
   ctx.fillText(`Ура, вы победили!`, CLOUD_X + GAP * 2, CLOUD_Y + FONT_GAP);
   ctx.fillText(`Список результатов:`, CLOUD_X + GAP * 2, CLOUD_Y + FONT_GAP + FONT_SIZE);
-
-  let COLUMN_X = CLOUD_X + GAP + COLUMN_WIDTH;
+  // Гистограмма
+  const MAX_ELEMENT = times.reduce(function (maxTime = times[0], time) {
+    return time > maxTime ? time : maxTime;
+  });
 
   const colors = names.map(function (name) {
     let color = `hsl(240, ${Math.round(Math.random() * 100)}%, 50%)`;
 
-    if (name === `Вы`) {
-      color = `rgb(250, 0, 0)`;
-    }
-
-    return color;
+    return name === `Вы` ? `rgb(250, 0, 0)` : color;
   });
 
-  const MAX_ELEMENT = getMaxElement(times);
+  let COLUMN_X = CLOUD_X + GAP + COLUMN_WIDTH;
 
   for (let i = 0; i < times.length; i++) {
     let columnHeight = times[i] * COLUMN_MAX_HEIGHT / MAX_ELEMENT;
     let COLUMN_Y = GAP + CLOUD_HEIGHT - COLUMN_WIDTH - columnHeight;
     renderRect(ctx, colors[i], COLUMN_X, COLUMN_Y, COLUMN_WIDTH, columnHeight);
+
+    // Текст гистограммы
+    ctx.fillStyle = `#000`;
+    ctx.fillText(names[i], COLUMN_X, GAP + CLOUD_HEIGHT - COLUMN_WIDTH + FONT_SIZE);
+    ctx.fillText(Math.round(times[i]), COLUMN_X, COLUMN_Y - GAP);
+
     COLUMN_X += COLUMN_WIDTH + COLUMN_GAP;
   }
 };
