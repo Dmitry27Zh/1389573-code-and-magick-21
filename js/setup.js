ebe65b52dbfc7817c19fc3fd9ca107a05ee7b69e
Dmitry <dmitry27zh@gmail.com>
2020-09-26 22:40:00 +0300

    добавлена настройка персонажа

---
+++ js/setup.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const COATCOLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const EYESCOLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const WIZARDS_QUANTITY = 4;
+
+const getRandomItem = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const generateWizards = function () {
+  let wizards = [];
+  for (let i = 1; i <= WIZARDS_QUANTITY; i++) {
+    let wizard = {};
+    wizard.name = `${getRandomItem(NAMES)} ${getRandomItem(SURNAMES)}`;
+    wizard.coatColor = `${getRandomItem(COATCOLORS)}`;
+    wizard.eyeColor = `${getRandomItem(EYESCOLORS)}`;
+    wizards.push(wizard);
+  }
+  return wizards;
+};
+
+const createWizardElement = function (wizard) {
+  const wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
+  const wizardElement = wizardTemplate.cloneNode(true);
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyeColor;
+  return wizardElement;
+};
+
+const addWizardsElements = function () {
+  const wizards = generateWizards();
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < wizards.length; i++) {
+    fragment.appendChild(createWizardElement(wizards[i]));
+  }
+  const similarList = document.querySelector(`.setup-similar-list`);
+  similarList.appendChild(fragment);
+};
+
+const openSetup = function () {
+  const setup = document.querySelector(`.setup`);
+  const setupSimilar = setup.querySelector(`.setup-similar`);
+  setup.classList.remove(`hidden`);
+  setupSimilar.classList.remove(`hidden`);
+  addWizardsElements();
+};
+
+openSetup();
+
+
